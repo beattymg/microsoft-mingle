f6f24baf1c32934c4956bc84696a47399d6a9c5f
Matthew Beatty <beattymg@gmail.com>
2017-07-25 15:40:33 -0700

    shows correct profile info

--- client/profile.js
+++ client/profile.js
@@ -1,10 +1,11 @@
 Template.profile.helpers({
     profile: function() {
-        Session.set('userId', "testing1@google.com");
+        // Session.set('userId', "testing1@google.com");
         console.log("session userid is " + Session.get('userId'));
         var prof = Profiles.find({ 'userId': Session.get('userId') }).fetch();
         console.log(prof);
-        return Profiles.find({ 'userId': Session.get('userId') }).fetch();
+        // return Profiles.find({ 'userId': Session.get('userId') }).fetch();
+        return Profiles.findOne({ 'userId': Session.get('userId') });
         // return Profiles.findOne();
     }
 });
